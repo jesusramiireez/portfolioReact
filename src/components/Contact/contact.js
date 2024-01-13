57bf856becff7bb459ec03cbf9936b840e5b0777
jesusramiireez <jrm01@iesemilidarder.com>
2024-01-13 20:16:32 +0100

    Implementado Modal para gestionar las alertas de los mensajes

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -7,7 +7,7 @@ import emailjs from '@emailjs/browser';
 
 const Contact = () => {
     const form = useRef();
-    const [alertMessage, setAlertMessage] = useState("Please fill out the form and click 'Send'.");
+    const [modalVisible, setModalVisible] = useState(false);
 
     const sendEmail = (e) => {
         e.preventDefault();
@@ -15,12 +15,16 @@ const Contact = () => {
         emailjs.sendForm('service_t90wycx', 'template_n1d0r0h', form.current, 'LZg6FMhDaQkTfD8Gs')
             .then((result) => {
                 console.log(result.text);
-                setAlertMessage('Message sent successfully!');
+                setModalVisible(true);
             })
             .catch((error) => {
                 console.error(error.text);
-                setAlertMessage('Error sending message. Please try again.');
+                setModalVisible(true);
             });
+    };
+
+    const closeModal = () => {
+        setModalVisible(false);
     };
 
     return (
@@ -29,17 +33,22 @@ const Contact = () => {
                 <h2 className='contactTitle'>&lt; Contact Me /&gt;</h2>
                 <span className='contactDesc'>Get in touch with me using the contact form below.</span>
                 <form className='contactForm' ref={form} onSubmit={sendEmail}>
-                    <input type='text' className='name' placeholder='Name' name='from_name' />
-                    <input type='email' className='email' placeholder='Email' name='from_email' />
-                    <textarea name="message" className="message" rows="5" placeholder='Your message here'></textarea>
+                    {/* ... (tu código de formulario) */}
                     <button type="submit" value="Send" className='formBtn'>Send</button>
                     <div className='socialMedia'>
                         <FontAwesomeIcon icon={faXTwitter} className='iconStyle' />
                         <FontAwesomeIcon icon={faInstagram} className='iconStyle' />
                     </div>
                 </form>
-                <div className={`alert ${alertMessage.includes('successfully') ? 'success' : 'error'}`}>{alertMessage}</div>
             </div>
+            {modalVisible && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <span className="close" onClick={closeModal}>&times;</span>
+                        <p>Message sent successfully!</p>
+                    </div>
+                </div>
+            )}
         </section>
     );
 }
