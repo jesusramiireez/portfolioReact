2acffced7ca1d2659942f6f87674a1b8637e70c5
jesusramiireez <jrm01@iesemilidarder.com>
2024-01-13 20:18:43 +0100

    Estilos editados

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -16,6 +16,7 @@ const Contact = () => {
             .then((result) => {
                 console.log(result.text);
                 setModalVisible(true);
+                form.current.reset();
             })
             .catch((error) => {
                 console.error(error.text);
@@ -33,22 +34,24 @@ const Contact = () => {
                 <h2 className='contactTitle'>&lt; Contact Me /&gt;</h2>
                 <span className='contactDesc'>Get in touch with me using the contact form below.</span>
                 <form className='contactForm' ref={form} onSubmit={sendEmail}>
-                    {/* ... (tu código de formulario) */}
+                    <input type='text' className='name' placeholder='Name' name='from_name' />
+                    <input type='email' className='email' placeholder='Email' name='from_email' />
+                    <textarea name="message" className="message" rows="5" placeholder='Your message here'></textarea>
                     <button type="submit" value="Send" className='formBtn'>Send</button>
+                    {modalVisible && (
+                        <div className="modal">
+                            <div className="modal-content">
+                                <span className="close" onClick={closeModal}>&times;</span>
+                                <p>Message sent successfully!</p>
+                            </div>
+                        </div>
+                    )}
                     <div className='socialMedia'>
                         <FontAwesomeIcon icon={faXTwitter} className='iconStyle' />
                         <FontAwesomeIcon icon={faInstagram} className='iconStyle' />
                     </div>
                 </form>
             </div>
-            {modalVisible && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <span className="close" onClick={closeModal}>&times;</span>
-                        <p>Message sent successfully!</p>
-                    </div>
-                </div>
-            )}
         </section>
     );
 }
