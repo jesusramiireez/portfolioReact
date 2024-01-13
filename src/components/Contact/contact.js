243961c85f63d3e78deeef849b8d6d1675b5f5d0
jesusramiireez <jrm01@iesemilidarder.com>
2024-01-13 20:12:22 +0100

    Alertas implementadas al enviar un mensaje de contacto

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -23,8 +23,8 @@ const Contact = () => {
                 <h2 className='contactTitle'>&lt; Contact Me /&gt;</h2>
                 <span className='contactDesc'>Get in touch with me using the contact form below.</span>
                 <form className='contactForm' ref={form} onSubmit={sendEmail}>
-                    <input type='text' className='name' placeholder='Name' name='your_name'/>
-                    <input type='email' className='email' placeholder='Email' name='your_email'/>
+                    <input type='text' className='name' placeholder='Name' name='from_name'/>
+                    <input type='email' className='email' placeholder='Email' name='from_email'/>
                     <textarea name="message" className="message" rows="5" placeholder='Your message here'></textarea>
                     <button type="submit" value="Send" className='formBtn'>Send</button>
                     <div className='socialMedia'>
